aea85db7ffbbb34b4263568a08241e508da5f0fe
Kaami Dev <kaami@kaamidev.com>
2020-11-07 16:47:18 -0500

    look for link

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// message template functions for reples
+const createMessage = (desc, name, image) => {
+	return new Discord.MessageEmbed().setAuthor(name, image).setColor('#00D632').setDescription(desc);
+};
+
+const createErr = (desc, name, image) => {
+	return new Discord.MessageEmbed().setAuthor(name, image).setColor('#F8453C').setDescription(desc);
+};
+
 // listen for discord commands
 client.on('message', (message) => {
 	// check for prefix in command
@@ -37,6 +46,36 @@ client.on('message', (message) => {
 
 		// check for specific commands
 		if (message.content === 'help') {
+			// display help embed
+			message.channel.send(
+				new Discord.MessageEmbed()
+					.setColor('#5BD5B8')
+					.setTitle('Grammarly Bot')
+					.setDescription('The perks of grammarly from right here on discord!')
+					.setThumbnail('https://imgur.com/lh3fO0H.png')
+					.addField(
+						'How to Use',
+						'`g!correct <raw pastebin>` - Bot will scan the link for errors and DM you a corrected text file.'
+					)
+					.setTimestamp()
+					.setFooter('Grammarly Bot', 'https://imgur.com/lh3fO0H.png')
+			);
+		} else if (message.content.split(' ')[0] === 'correct') {
+			// extract link from command
+			let link = message.content.split(' ')[1];
+
+			// make sure link exists
+			if (link) {
+			} else {
+				// if invalid link, send error
+				message.channel.send(
+					createErr(
+						'Error correcting text.\nInvalid link was provided.',
+						message.member.user.tag,
+						message.member.user.avatarURL()
+					)
+				);
+			}
 		}
 	}
 });
